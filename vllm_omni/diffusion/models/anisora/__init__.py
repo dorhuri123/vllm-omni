7bc3014d50c09dea44a7454d75249e250293ecaf
dorh <dorh@deepsea.team>
2026-01-19 20:55:15 +0200

    Add proper exports to anisora __init__.py following vLLM-Omni conventions

--- vllm_omni/diffusion/models/anisora/__init__.py
+++ vllm_omni/diffusion/models/anisora/__init__.py
@@ -1,2 +1,21 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+from .pipeline_anisora import (
+    AniSoraPipeline,
+    get_anisora_post_process_func,
+    get_anisora_pre_process_func,
+)
+from .pipeline_anisora_i2v import (
+    AniSoraI2VPipeline,
+    get_anisora_i2v_post_process_func,
+    get_anisora_i2v_pre_process_func,
+)
+
+__all__ = [
+    "AniSoraPipeline",
+    "get_anisora_post_process_func",
+    "get_anisora_pre_process_func",
+    "AniSoraI2VPipeline",
+    "get_anisora_i2v_post_process_func",
+    "get_anisora_i2v_pre_process_func",
+]
